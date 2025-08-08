99dd1202b88c1a43f801410b0daf2f37f10f0638
arman-0603 <arukhan086@gmail.com>
2025-08-08 18:15:28 +0530

    2nd commit

--- app/src/main/java/com/example/fitnesstracker/view/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fitnesstracker
+package com.example.fitnesstracker.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             FitnessTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RealTimeLocationScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnessTrackerTheme {
-        Greeting("Android")
-    }
-}
